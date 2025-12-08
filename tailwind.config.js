b35976baaa0f8c513a5d17dcd65cf318808d5347
Thiago ferreira <thiagoferreira.113@gmail.com>
2025-12-08 17:13:53 -0300

    chore: setup project environment

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#f0fdfa',
+          100: '#ccfbf1',
+          500: '#14b8a6',
+          600: '#0d9488',
+          700: '#0f766e',
+          900: '#134e4a',
+        }
+      },
+    },
+  },
+  plugins: [],
+}
